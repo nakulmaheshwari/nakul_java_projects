cd4300d7e8a18140108a10e39cfed4d175abfe96
nakulmaheshwari <62350319+nakulmaheshwari@users.noreply.github.com>
2023-01-23 10:11:33 -0500

    Delete UserService directory

--- UserService/src/main/java/com/nakul/UserService/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.nakul.UserService.repositories;
-import com.nakul.UserService.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-}
